f099dc64986fa4203a2bbb45ddafc1bc4d624ac1
toshiaki181 <toshiaki181@gmail.com>
2015-01-12 11:57:54 +0900

    3.4ファイルを操作してみよう　完了 P62/3はコードがなく削除不要でした。

--- Omikuji/src/makeApplication/omikuji/OmikujiActivity.java
+++ Omikuji/src/makeApplication/omikuji/OmikujiActivity.java
@@ -2,8 +2,7 @@ package makeApplication.omikuji;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 public class OmikujiActivity extends Activity {
 
@@ -11,5 +10,8 @@ public class OmikujiActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		// 文字を表示する
+		TextView tv = (TextView) findViewById(R.id.hello_view);
+		tv.setText("おみくじアプリ");
 	}
 }
