e296cdd24fd84f336a63226be5245103dcd3c250
toshiaki181 <toshiaki181@gmail.com>
2015-01-12 18:45:19 +0900

    第5章　アプリに画像を組み込もう　完了 画像が縦に揺れるところまで！

--- Omikuji/src/makeApplication/omikuji/OmikujiActivity.java
+++ Omikuji/src/makeApplication/omikuji/OmikujiActivity.java
@@ -4,6 +4,12 @@ import java.util.Random;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class OmikujiActivity extends Activity {
@@ -11,8 +17,8 @@ public class OmikujiActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-
+		setContentView(R.layout.omikuji);
+		/*
 		TextView tv = (TextView) findViewById(R.id.hello_view);
 		
 		// くじ番号の取得
@@ -31,5 +37,30 @@ public class OmikujiActivity extends Activity {
 		String str = omikujiShelf[number];
 		
 		tv.setText(str);
+		*/
+	}
+	
+	public void onButtonClick(View v) {
+		TranslateAnimation translate = new TranslateAnimation(0, 0, 0, -100);
+		translate.setRepeatMode(Animation.REVERSE);
+		translate.setRepeatCount(5);
+		translate.setDuration(100);
+		
+		ImageView imageview = (ImageView)findViewById(R.id.imageView1);
+		
+		RotateAnimation rotate = new RotateAnimation(0, -36, imageview.getWidth() / 2, imageview.getHeight() / 2);
+		rotate.setDuration(200);
+		
+		AnimationSet set = new AnimationSet(true);
+		set.addAnimation(rotate);
+		set.addAnimation(translate);
+		
+		imageview.startAnimation(set);
+		
+		/*
+		ImageView image = new ImageView(this);
+		image.setImageResource(R.drawable.result1);
+		setContentView(image);
+		*/		
 	}
 }
