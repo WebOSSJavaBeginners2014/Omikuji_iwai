b047cd77085bae2d4ec4e617eabf3a2c22e32697
toshiaki181 <toshiaki181@gmail.com>
2015-01-12 22:54:19 +0900

    第7章　7.1メニューをつけてみよう　完了

--- Omikuji/src/makeApplication/omikuji/OmikujiActivity.java
+++ Omikuji/src/makeApplication/omikuji/OmikujiActivity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OmikujiActivity extends Activity {
 	
@@ -113,5 +117,25 @@ public class OmikujiActivity extends Activity {
 			}
 		}
 		return super.onTouchEvent(event);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		Toast toast = Toast.makeText(this, item.getTitle(), Toast.LENGTH_LONG);
+		toast.show();
+		
+		return super.onOptionsItemSelected(item);
 	}
 }
